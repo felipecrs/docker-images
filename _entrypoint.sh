5c20103371d7f839b91d375fe0abfa132207edbd
Felipe Santos <felipecassiors@gmail.com>
2021-02-02 11:52:10 -0300

    Remove fixuid
    As this is not the purpose of this container.

--- _entrypoint.sh
+++ _entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-UID=${1}
-GID=${2}
+readonly _UID=${1?"UID is mandatory. Usage: $0 UID GID"}
+readonly _GID=${2?"GID is mandatory. Usage: $0 UID GID"}
 shift
 shift
 
-S6_CMD_WAIT_FOR_SERVICES=1 exec /init setpriv "--reuid=$UID" "--regid=$GID" --init-groups -- "$@"
+S6_CMD_WAIT_FOR_SERVICES=1 exec -- /init setpriv "--reuid=$_UID" "--regid=$_GID" --init-groups -- "$@"
