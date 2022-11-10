65111a232c5d58f29606173a154ad31f0fe4c055
Felipe Santos <felipecassiors@gmail.com>
2022-11-10 17:46:36 -0300

    Allow to start SSHD with SSHD_ENABLED=true

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -12,7 +12,7 @@ uid="$(id -u)"
 if [[ "${uid}" -eq 0 ]]; then
     # If running as root, simply execute s6-overlay
     export USER="root"
-    cmd=(/init)
+    cmd=(/init_as_root)
 else
     # Otherwise, fix uid and gid, run s6-overlay as root and then drop
     # privileges back to the user
