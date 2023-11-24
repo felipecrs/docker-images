70ec5925a5effb1a424865a8d97080a14d583032
Felipe Santos <felipecassiors@gmail.com>
2023-11-24 17:21:50 -0300

    Revert "Upgrade s6-overlay from v2 to v3 (#78)"
    This reverts commit b07484484ed748bfb27e53bd20d2b099b959836d, due to
    https://github.com/just-containers/s6-overlay/issues/558.

--- rootfs/etc/optional-services.d/sshd/check_readiness.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null jenkins@127.0.0.1 true &>/dev/null
--- rootfs/etc/services.d/dind/check_readiness.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec docker version &>/dev/null
