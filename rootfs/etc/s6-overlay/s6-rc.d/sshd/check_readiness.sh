af851191c48ff09bc2aa1b79bb12cb1053338852
Felipe Santos <felipecassiors@gmail.com>
2024-03-11 10:14:49 -0300

    Use new style s6-overlay services

---
+++ rootfs/etc/s6-overlay/s6-rc.d/sshd/check_readiness.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+exec ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+    "${NON_ROOT_USER?}@127.0.0.1" true &>/dev/null
--- scripts/init_as_root.sh
+++ scripts/init_as_root.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 #
-# This script needs to be compiled with shc enabling the suid bit, so we have
-# privileges to operate as root.
+# This script needs to be compiled with shc enabling the suid bit, so even
+# though the container starts as a non-root user, it operates as if it was
+# ran as root.
 #
 # /init: s6-overlay
 
-set -eu
-
-# This can probably be improved:
-# https://github.com/just-containers/s6-overlay/issues/394#issuecomment-1690769622
-if [[ "${SSHD_ENABLED:-false}" == true ]]; then
-    mv -f /etc/optional-services.d/sshd /etc/services.d/sshd
-fi
-
-# check if docker socket is mounted, meaning we are running in docker on docker mode
-if [[ -S "/var/run/docker.sock" ]]; then
-    # setup docker-on-docker-shim by default when running in docker on docker mode
-    docker_path=$(command -v docker)
-    mv -f "${docker_path}" "${docker_path}.orig"
-    dond_path=$(command -v dond)
-    mv -f "${dond_path}" "${docker_path}"
-    unset docker_path dond_path
-else
-    mv -f /etc/optional-services.d/dind /etc/services.d/dind
-fi
-
-exec -- /init "$@"
+exec /init "$@"
