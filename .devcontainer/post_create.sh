a8deaf18265da79917d8468b52d9211c2a24c228
Felipe Santos <felipecassiors@gmail.com>
2024-03-20 02:13:24 -0300

    Fix devcontainer not running entrypoint

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,10 +2,27 @@
 
 set -ex
 
-pkgx install k3d@5 helmfile@0.162 werf@1 kubectl@1
+# quiets the output a little
+export CI=true
 
-if [[ "${CI:-false}" == false ]]; then
-    pkgx install npm@10 node@20 hadolint@2 act@0.2
+pkgx install \
+    k3d@5 \
+    helmfile@0.162 \
+    werf@1 \
+    kubectl@1 \
+    hadolint@2 \
+    act@0.2
+
+if [[ "${GITHUB_ACTIONS:-false}" == false ]]; then
+    # This fails in GitHub Actions, but the default node and npm version works
+    # anyway
+    pkgx install \
+        node@20 \
+        npm@10
 fi
 
+node --version
+npm --version
+
 npm install --global @devcontainers/cli@0.57
+devcontainer --version
--- devcontainer/rootfs/etc/s6-overlay/s6-rc.d/sshd/condition.sh
+++ devcontainer/rootfs/etc/s6-overlay/s6-rc.d/sshd/condition.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-test "${SSHD_ENABLED:-false}" == true
+set -eu
+
+if [[ "${SSHD_ENABLED:-false}" == false ]]; then
+    exit 1
+fi
+
+# Needed for situations like:
+#   docker run --rm -v /ssh-command:/ssh-command ubuntu /ssh-command/get.sh
+echo "${USER}" >/ssh-command/user
+
+exit 0
