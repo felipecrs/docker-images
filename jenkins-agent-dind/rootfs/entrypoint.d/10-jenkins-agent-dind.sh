88b4299542f1d46ea49c19ab9a2ab5381cb9628a
Felipe Santos <felipecassiors@gmail.com>
2025-02-10 12:19:47 -0300

    Fix jenkins-agent-dind not working with direct connection

--- jenkins-agent-dind/rootfs/entrypoint.d/10-jenkins-agent-dind.sh
+++ jenkins-agent-dind/rootfs/entrypoint.d/10-jenkins-agent-dind.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 
 if [[ $# -eq 0 ]]; then
-    # If JENKINS_URL is preset, assume we are running as a Kubernetes Pod template agent
-    if [[ -n "${JENKINS_URL:-}" ]]; then
+    # If JENKINS_AGENT_NAME is preset, assume we are running as a Kubernetes Pod template agent
+    if [[ -n "${JENKINS_AGENT_NAME:-}" ]]; then
         set -- jenkins-agent
     fi
 fi
